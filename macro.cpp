55457f62118d73384bdaf568fc44317dbbe9133f
g-alessandra <80726328+g-alessandra@users.noreply.github.com>
2021-09-18 22:12:24 -0300

    Update macro.cpp

--- macro.cpp
+++ macro.cpp
@@ -33,9 +33,55 @@ int main(){
     vector<string> lines;
     string line;
 
-    while (getline(input_file, line)){
+    while (getline(input_file, line)){ //le linha a linha
+        lines.push_back(line);
+    }
+
+    while (getline(input_file, line) && (line.compare(0,7,"SomaMem") != 0)){ //le até encontrar SomaMen
         lines.push_back(line);
     }
 
     for (const auto &i : lines)
         cout << i << endl;
+
+
+
+//------ funcionou essa parte
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using std::cout; using std::cerr;
+using std::endl; using std::string;
+using std::ifstream; using std::vector;
+using std::ofstream;
+
+int main(){
+    string filename("file.txt");
+    vector<string> lines;
+    string line;
+    
+    
+    ofstream f_out; //gera Arquivo de saida
+    f_out.open("teste.txt");
+
+    if(! f_out.good()){ //verifica se houve erro na criação do arquivo
+        printf("Erro ao criar arquivo!");
+        exit(1);
+    }
+    
+
+    ifstream input_file(filename);
+    input_file.is_open();
+
+
+    while (getline(input_file, line) && (line.compare(0,13,"SomaMem MACRO") != 0)){
+        f_out << line + "\n";
+    }
+
+    for (const auto &i : lines)
+        cout << i << endl;
+
+    input_file.close();
+    return EXIT_SUCCESS;
+}
